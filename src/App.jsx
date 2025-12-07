d855f7cd23e8acec9c35c16906a66abdef0d9aee
imakash-24 <imakash2409@gmail.com>
2025-12-07 21:32:28 +0530

    css styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import './App.css'
 import Banner from './components/Banner'
 import ContactForm from './components/ContactForm'
 
-
 function App() {
-
   return (
     <div className='container'>
       <Banner/>
       <ContactForm/>
     </div>
-           
- 
   )
 }
 
